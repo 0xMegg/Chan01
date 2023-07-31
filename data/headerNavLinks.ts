ef9e01d1f6e5c1a818d647aab7ca382f94f7fc35
김준엽 <finero91@gmail.com>
2023-07-31 19:52:09 +0900

    fixing navBar

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,8 +1,8 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/test', title: 'Test' },
 ]
 
 export default headerNavLinks
